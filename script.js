264ec1ac921d2e0fe77a4b2927aac52faeab5f2f
mujukno1-ship-it <mujukno1@gmail.com>
2025-11-22 15:56:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('basic version');
+document.getElementById("startBtn").onclick = async () => {
+    const motion = document.getElementById("motionFile").files[0];
+    const image = document.getElementById("imageFile").files[0];
+
+    if (!motion || !image) {
+        alert("두 파일 모두 업로드하세요.");
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append("motion", motion);
+    formData.append("image", image);
+
+    const res = await fetch("http://localhost:5000/generate", {
+        method: "POST",
+        body: formData
+    });
+
+    const blob = await res.blob();
+    const url = URL.createObjectURL(blob);
+
+    document.getElementById("resultVideo").src = url;
+};
